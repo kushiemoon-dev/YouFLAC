c0f2bdf4d271d68bd18e0c3ca43aef2238b5c3e8
kushie <kushie.dev@icloud.com>
2026-04-11 13:57:05 +0000

    docs: add animated showcase GIF and Remotion project

---
+++ promo/youflac-showcase/src/Root.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Composition } from "remotion";
+import { Showcase, SCREENS, SCREEN_DURATION, TRANSITION } from "./Showcase";
+
+const totalFrames = 50 + SCREENS.length * (SCREEN_DURATION - TRANSITION) + 60;
+
+export const Root: React.FC = () => {
+  return (
+    <Composition
+      id="Showcase"
+      component={Showcase}
+      durationInFrames={totalFrames}
+      fps={30}
+      width={1280}
+      height={800}
+    />
+  );
+};
